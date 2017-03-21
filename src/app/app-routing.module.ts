de37bb0ed8116e7372fff46294439f530a62b35d
Anthony Archambeau <mail@anthony-archambeau.ovh>
2017-03-21 22:41:27 +0100

    Suppresion de code inutile + ajout de photo de chat

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,4 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
-import { StoryService } from './story.service';
-import { AppComponent } from './app.component';
 import { DetailStoryComponent } from './detail-story/detail-story.component';
 import { StoriesComponent } from './stories-component/stories.component';
 import {  Routes,RouterModule}   from '@angular/router';
